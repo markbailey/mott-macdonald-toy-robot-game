0ac185fe3dedcb071bfff88f2160c6012316a4db
Mark Bailey <markbailey78@yahoo.co.uk>
2023-05-07 23:23:59 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { Fragment, useState } from 'react';
 import GameView from './views/GameView';
+import { mount } from './utilities/show';
+import MenuView from './views/MenuView';
 
 function App() {
-  return <GameView />;
+  const [view, setView] = useState('menu');
+  const [level, setLevel] = useState(0);
+
+  const onStartGame = (level: number) => {
+    setLevel(level);
+    setView('game');
+  };
+
+  const onExitGame = () => {
+    setLevel(0);
+    setView('menu');
+  };
+
+  return (
+    <Fragment>
+      {mount(view === 'menu', <MenuView onStartGame={onStartGame} />)}
+      {mount(view === 'game', <GameView startLevel={level} onExit={onExitGame} />)}
+    </Fragment>
+  );
 }
 
 export default App;
