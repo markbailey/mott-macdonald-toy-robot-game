7cc6f9a30575071a60f0803421791ff32a2f55e7
Mark Bailey <markbailey78@yahoo.co.uk>
2023-05-08 11:52:27 +0100

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,20 +2,11 @@ import { ButtonHTMLAttributes, ForwardedRef, forwardRef } from 'react';
 import classNames from 'classnames';
 import styles from '../assets/stylesheets/components/button.module.scss';
 
-export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
-  color?: 'danger';
-  iconOnly?: boolean;
-};
+export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button = forwardRef((props: ButtonProps, ref: ForwardedRef<HTMLButtonElement>) => {
-  const { className, color, iconOnly, ...otherProps } = props;
-  const newClassName = classNames(
-    styles.button,
-    color && styles[color],
-    iconOnly && styles.iconOnly,
-    className
-  );
-
+  const { className, ...otherProps } = props;
+  const newClassName = classNames(styles.button, className);
   return <button type="button" {...otherProps} ref={ref} className={newClassName} />;
 });
 
